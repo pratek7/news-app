17bf8b5ee7fcaa43171e28fcd5c3a5e024174a18
pratek7 <pratikjungkarki123@gmail.com>
2023-09-10 12:26:45 +0545

    dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,15 @@ export default function RootLayout({
   return (
     <html>
       <body>
-        <div className="bg-gray-100 transition-all dark:bg-zinc-900 duration-700">
-          <Header />
-          <div className="max-w-6xl mx-auto">{children}</div>
-          <footer className="text-center m-20 text-3xl font-extrabold animate-bounce">
-            &copy; Develop By: Pratik Karki, Developer
-          </footer>
-        </div>
+        <Providers>
+          <div className="bg-gray-100 transition-all dark:bg-zinc-900 duration-700">
+            <Header />
+            <div className="max-w-6xl mx-auto">{children}</div>
+            <footer className="text-center m-20 text-3xl font-extrabold animate-bounce">
+              &copy; Develop By: Pratik Karki, Developer
+            </footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
